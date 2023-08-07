84ff450f74799947078fae3f63f75aa73668cbcd
William Thomas <wt4@icloud.com>
2023-08-06 21:47:14 -0400

    First commit for caesar cypher

--- cipher.py
+++ cipher.py
@@ -1,1 +1,17 @@
-# add your code here
+#define the sentence with user input
+sentence = input("Please enter a sentence: ")
+#set the shift
+shift = 5
+
+#define the alphabet simply by using a string
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+#create a shifted alphabet 
+shifted = alphabet[shift:] + alphabet[:shift]
+
+#make a translation table to map alphabet to shifted alphabet
+table = str.maketrans(alphabet, shifted)
+
+encrypted = sentence.translate(table)
+
+print("The encrypted sentence is:", encrypted)
