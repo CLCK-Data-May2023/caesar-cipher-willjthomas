b9a23f1938fa7bc8bb1dedcdfea1ffaaf270e5f5
William Thomas <wt4@icloud.com>
2023-08-06 21:57:39 -0400

    Added code to convert user input to lowercase

--- cipher.py
+++ cipher.py
@@ -1,5 +1,4 @@
-#define the sentence with user input
-sentence = input("Please enter a sentence: ")
+
 #set the shift
 shift = 5
 
@@ -12,6 +11,8 @@ shifted = alphabet[shift:] + alphabet[:shift]
 #make a translation table to map alphabet to shifted alphabet
 table = str.maketrans(alphabet, shifted)
 
+#define the sentence with user input
+sentence = input("Please enter a sentence: ").lower()
 encrypted = sentence.translate(table)
 
 print("The encrypted sentence is:", encrypted)
